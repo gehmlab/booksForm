97a899acd7674d10dbf4621f7591c6412ef247d4
gehmlab <gehm@inbox.ru>
2025-03-17 23:03:26 +0300

    Toggle favorite book

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,15 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteBook } from '../../redux/books/actionCreators';
+import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
+import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
 import './BookList.css';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
-  
+
   const handleDeleteBook = (id) => {
     dispatch(deleteBook(id));
-  }
-  
+  };
+
+  const handleToggleFavorite = (id) => {
+    dispatch(toggleFavorite(id));
+  };
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -23,6 +28,13 @@ const BookList = () => {
                 {++i}. {book.title} by <strong>{book.author}</strong>
               </div>
               <div className="book-actions">
+                <span onClick={() => handleToggleFavorite(book.id)}>
+                  {book.isFavorite ? (
+                    <BsBookmarkStarFill className="star-icon" />
+                  ) : (
+                    <BsBookmarkStar className="star-icon" />
+                  )}
+                </span>
                 <button onClick={() => handleDeleteBook(book.id)}>Delete</button>
               </div>
             </li>
--- frontend/src/redux/books/actionCreators.js
+++ frontend/src/redux/books/actionCreators.js
@@ -7,9 +7,22 @@ export const addBook = (newBook) => {
   };
 };
 
+// не явный возврат объекта
+// export const addBook = (newBook) => ({
+//     type: a.ADD_BOOK,
+//     payload: newBook,
+//   });
+
 export const deleteBook = (id) => {
   return {
     type: a.DELETE_BOOK,
     payload: id,
   };
 };
+
+export const toggleFavorite = (id) => {
+  return {
+    type: a.TOGGLE_FAVORITE,
+    payload: id,
+  }
+}
