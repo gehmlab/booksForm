023e5a937b3b1eb9be889c2a08393adc4b2fd7c2
gehmlab <gehm@inbox.ru>
2025-03-19 22:24:20 +0300

    Filter Redux slice and filter by title

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,9 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setTitleFilter, resetFilters, selectTitleFilter } from '../../redux/slices/filterSlice';
 import './Filter.css';
 
 const Filter = () => {
+  const dispatch = useDispatch();
+  const titleFilter = useSelector(selectTitleFilter);
+
+  const handleTitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value));
+  };
+
+  const handleResetFilters = () => {
+    dispatch(resetFilters())
+  }
+
   return (
     <div className="app-block filter">
-      <h2>Filter</h2>
+      <div className="filter-group">
+        <div className="filter-group">
+          <input 
+            type="text" 
+            value={titleFilter}
+            placeholder="Filter by title..." 
+            onChange={handleTitleFilterChange} 
+          />
+      </div>
+        <button type="button" onClick={handleResetFilters}>Reset Filters</button>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/redux/slices/filterSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  title: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setTitleFilter: (state, action) => {
+      // в Slice можно мутировать состояние благодаря библиотеке immer
+      // You can mutate state thanks to Immer library
+      state.title = action.payload ;
+      
+      // You can also return new state as usually
+      // return { ...state, title: action.payload };
+    },
+    resetFilters: (state) => {
+      return {...initialState}
+    }
+  },
+});
+
+export const { setTitleFilter, resetFilters } = filterSlice.actions;
+
+export const selectTitleFilter = (state) => state.filter.title;
+
+export default filterSlice.reducer;
