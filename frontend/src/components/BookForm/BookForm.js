befaee23c5102cd953f998f16f31ceddb4ecd043
gehmlab <gehm@inbox.ru>
2025-03-20 21:39:50 +0300

    Add random book via API

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import { addBook } from '../../redux/slices/booksSlice';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
@@ -12,7 +13,7 @@ const BookForm = () => {
 
   const handleAddRandomBook = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
-    const randomBook = booksData[randomIndex]; 
+    const randomBook = booksData[randomIndex];
     dispatch(addBook(createBookWithID(randomBook)));
   };
 
@@ -20,9 +21,20 @@ const BookForm = () => {
     e.preventDefault();
 
     if (title && author) {
-      dispatch(addBook(createBookWithID({title, author})));
+      dispatch(addBook(createBookWithID({ title, author })));
       setTitle('');
       setAuthor('');
+    }
+  };
+
+  const handleAddRandomBookViaAPI = async () => {
+    try {
+      const res = await axios.get('http://localhost:4000/random-book');
+      if (res?.data?.title && res?.data?.author) {
+        dispatch(addBook(createBookWithID(res.data)));
+      }
+    } catch (error) {
+      console.log('Error fetching random book', error);
     }
   };
 
@@ -46,6 +58,9 @@ const BookForm = () => {
         <button type="submit">Add Book</button>
         <button type="button" onClick={handleAddRandomBook}>
           Add Random
+        </button>
+        <button type="button" onClick={handleAddRandomBookViaAPI}>
+          Add Random via API
         </button>
       </form>
     </div>
