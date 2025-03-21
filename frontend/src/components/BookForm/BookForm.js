52576f43c05b4792383dccb1622f6ce690e4f40f
gehmlab <gehm@inbox.ru>
2025-03-21 23:56:58 +0300

    Added isLoading an spinner in the BookForm

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { FaSpinner } from 'react-icons/fa';
 import { addBook, fetchBook } from '../../redux/slices/booksSlice';
 import { setError } from '../../redux/slices/errorSlice';
 import createBookWithID from '../../utils/createBookWithID';
@@ -9,6 +10,7 @@ import './BookForm.css';
 const BookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
   const handleAddRandomBook = () => {
@@ -24,14 +26,18 @@ const BookForm = () => {
       dispatch(addBook(createBookWithID({ title, author }, 'manual')));
       setTitle('');
       setAuthor('');
-    }
-    else {
-      dispatch(setError('You must fill title an author!'))
+    } else {
+      dispatch(setError('You must fill title an author!'));
     }
   };
 
-  const handleAddRandomBookViaAPI = () => {
-    dispatch(fetchBook('http://localhost:4000/random-book'))
+  const handleAddRandomBookViaAPI = async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
+    } finally {
+      setIsLoading(false)
+    }
   };
 
   return (
@@ -55,8 +61,15 @@ const BookForm = () => {
         <button type="button" onClick={handleAddRandomBook}>
           Add Random
         </button>
-        <button type="button" onClick={handleAddRandomBookViaAPI}>
-          Add Random via API
+        <button type="button" onClick={handleAddRandomBookViaAPI} disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <span>Loading Book...</span>
+              <FaSpinner className="spinner" />
+            </>
+          ) : (
+            'Add Random via API'
+          )}
         </button>
       </form>
     </div>
